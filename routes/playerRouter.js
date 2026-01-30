502b8754b0ec357558aff72def411687920f5d74
Suraish Kirshan <sureshkamar3000@gmail.com>
2026-01-30 17:35:03 +0500

    Create player bid history section in playerProfile.ejs page | Create bulk upload button and feature in players.ejs and add player also

--- routes/playerRouter.js
+++ routes/playerRouter.js
@@ -5,9 +5,21 @@ import upload from '../middlewares/uploadMiddleware.js';
 import csrfProtection from '../middlewares/csrfMiddleware.js';
 import validator from '../middlewares/formValidator.js';
 import { registerSchema, loginSchema, playerRegisterSchema } from '../validators/authValidation.js'
+
+import multer from 'multer';
+import csv from 'csv-parser';
+import fs from 'fs';
+const uploadCsv = multer({ dest: 'uploads/' });
+
+const uploadDir = './uploads';
+if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir);
+}
+
 const router = Router();
 
-
+router.post('/bulk-upload', uploadCsv.single('csvFile'), playerController.handleBulkUpload);
+router.get('/bulk-upload', requireAdminAuth, playerController.renderBulkUpload);
 router.get('/playerslist', requireAdminOrOwner, playerController.renderAllPlayers);
 router.get('/profile/:id', requireAdminOrOwner, playerController.renderPlayerProfile);
 // router.get('/profile/delete/:id', requireAdminOrOwner, playerController.renderDelete);
